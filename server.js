52941b2db52fa47ab7bbcb75822a736d4e588461
Nova <xueshengyuan1118@hotmail.com>
2019-07-23 22:09:25 -0400

    Initialize server

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
+var app = express();
+var router = express.Router();
+var routes = require("./controllers");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+app.engine("handlebars",exphbs({defaultLayout : "main"}));
+app.set("view engine","handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//routes here
 
-mongoose.connect(MONGODB_URI);
+app.listen(PORT,function(){
+    console.log("App running on "+ PORT);
+    
+});
