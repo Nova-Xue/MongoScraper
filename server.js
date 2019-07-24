a4fe18193dbceb3004b188ffb031d2e3f538997e
Nova <xueshengyuan1118@hotmail.com>
2019-07-23 22:28:31 -0400

    Change server setting to use controllers instead of routes and create main handlebar and index handlebar

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ var db = require("./models");
 var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 var app = express();
-var router = express.Router();
-var routes = require("./routes");
+var routes = require("./controllers");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 app.engine("handlebars",exphbs({defaultLayout : "main"}));
 app.set("view engine","handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//routes here
+app.use(routes);
 
 app.listen(PORT,function(){
     console.log("App running on "+ PORT);
