7dd133d69de1e15a24e8e131f54610320043cc34
Nova <xueshengyuan1118@hotmail.com>
2019-07-25 20:47:48 -0400

    Change page layout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -53,13 +53,6 @@ $(function () {
             location.reload();
         });
     }
-    function renderForm(note){
-            if (note) {
-                //replace the note  card with form
-            }else{
-                // add new form
-            }
-    }
     $(".action").click(function(){
         buttonActions($(this).attr("data-value"));
     });
@@ -69,11 +62,4 @@ $(function () {
     $(".save-post").click(function(){
         saveById($(this).val());
     });
-    // $(document).on("click","card",function(){
-    //     renderForm($(this).attr("data-value"));
-    // });
-    // $(".new-note").click(function(e){
-    //     e.preventDefault();
-    //     $(this).parent().find("#notetitle")
-    // });
 });
